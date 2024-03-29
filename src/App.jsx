9bd354ca7cd7c91cedfa27d2187d4c57419ab687
alekssio <aleksandar.nikolic.sio@gmail.com>
2024-03-29 15:20:05 +0100

    add: deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <div className="w-full h-[100vh] bg-black/85 flex flex-col justify-center items-center">
-        <Router basename="/">
+        <Router basename="/samsung_replika_calculator">
           <Routes>
           <Route path="/" element={<BasicCalc />}/>
           <Route path="/s" element={<SpecialCalc />}/>
